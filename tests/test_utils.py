b604e6ebd57fcf1e69fdca06cbf3f37a2d05d440
Pablo Panero <pablo.panero@cern.ch>
2022-04-05 11:01:30 +0200

    global: remove __future__ and six imports

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2016-2019 CERN.
+# Copyright (C) 2016-2022 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Test API."""
-
-from __future__ import absolute_import, print_function
 
 from unittest.mock import patch
 
