d4e3444765e04c4ca3f0c05e5301b2f9a7342a4d
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 12:55:16 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2016-2018 CERN.
-# SPDX-FileCopyrightText: 2022 Graz University of Technology.
-# SPDX-License-Identifier: MIT
-
-"""Record indexer for Invenio."""
-
-from setuptools import setup
-
-setup()
